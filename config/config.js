fa18d7fff53452dafcba3afd4561306bcc2997e1
Francis Cruz <fcruz@blend.com>
2020-11-07 18:05:22 -0800

    undid config changes

--- config/config.js
+++ config/config.js
@@ -1,28 +1,28 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   dev: {
-    username: "fcruz",
-    password: null,
-    database: "develpment",
-    host: "127.0.0.1",
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
+    // username: "fcruz",
+    // password: null,
+    // database: "develpment",
+    // host: "127.0.0.1",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
   },
   test: {
     // "username": "root",
     // "password": null,
     // "database": "database_development",
     // "host": "127.0.0.1",
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
   },
   production: {
     // "username": "root",
     // "password": null,
     // "database": "database_development",
     // "host": "127.0.0.1",
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
   },
 };
