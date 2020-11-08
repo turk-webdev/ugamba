dd5161a12aedf61771ec3c1b60ad3748b4fbfa6d
Turk Erdin <ufkun@erdin.org>
2020-11-07 16:44:18 -0800

    created routes for CRUD poc

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ if (process.env.NODE_ENV === 'dev') {
 // const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
 const testsRouter = require('./routes/tests');
+const turkRoutes = require('./routes/turkDemo.js')
 
 // Instantiate the app
 const app = express();
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/tests', testsRouter);
+app.use('/turk', turkRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
