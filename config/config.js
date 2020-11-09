8911f244b65719d4015b9dce168cdd206c94d8ab
Turk Erdin <ufkun@erdin.org>
2020-11-09 10:08:49 -0800

    .

--- config/config.js
+++ config/config.js
@@ -1,25 +1,16 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: "root",
-    password: null,
-    database: "database_development",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
+ "development": {
+ "use_env_variable": "DATABASE_URL",
+ "dialect": "postgres"
+ },
+ "test": {
+ "use_env_variable": "DATABASE_URL",
+ "dialect": "postgres"
+ },
+ "production": {
+ "use_env_variable": "DATABASE_URL",
+ "dialect": "postgres"
+ }
 }
