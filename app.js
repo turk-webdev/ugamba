397e8bcece8e9022abaa7e214ade60f0f165f8ce
adamjbea <adamjbea@gmail.com>
2020-11-12 18:42:52 -0800

    test

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'dev') {
 const usersRouter = require('./routes/users');
 const testsRouter = require('./routes/tests');
 const gameRouter = require('./routes/game')
+const apiRouter = require('./routes/api');
 
 // Instantiate the app
 const app = express();
@@ -38,7 +39,8 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tests', testsRouter);
-app.use('/game', gameRouter)
+app.use('/game', gameRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
