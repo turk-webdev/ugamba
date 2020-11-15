dc8b05b1dbbf87baabb6dc7998b385eea7c60c78
Turk Erdin <ufkun@erdin.org>
2020-11-14 17:24:09 -0800

    Changed around card table to be a single table - easier

--- csv/scripts.sql
+++ csv/scripts.sql
@@ -1,14 +1,4 @@
-COPY card_value(id, value)
-FROM '/Users/turkerdin/Downloads/values.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY card_suit(id, suit)
-FROM '/Users/turkerdin/Downloads/suits.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY card(id, id_suit, id_value)
+COPY card(id, suit, suit_display, value, value_display)
 FROM '/Users/turkerdin/Downloads/cards.csv'
 DELIMITER ','
 CSV HEADER;
