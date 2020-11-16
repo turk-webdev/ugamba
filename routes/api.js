82dc680bf77750c8ff23106f77d9eea6fa698205
Turk Erdin <ufkun@erdin.org>
2020-11-16 14:08:54 -0800

    .

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const deckController = require('../controllers/deck.controller.js');
+const testController = require('../controllers/test.controller.js');
 
 // Deck API calls
 router.get('/deck/card/all', deckController.showAll);
 router.get('/deck/card/:id', deckController.showCard);
+
+// Test API calls
+router.get('/all/:table', testController.showAll);
 
 router.get('/', (request, response) => {
   response.send('Hello API');
