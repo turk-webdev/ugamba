e6e85d0e9fcf841e4c2d1175b8d3b3f64615a62b
Turk Erdin <ufkun@erdin.org>
2020-11-17 12:48:40 -0800

    Built & tested everything for initializing game deck

--- controllers/deck.controller.js
+++ controllers/deck.controller.js
@@ -1,4 +1,7 @@
 const Read = require('../classes/dbRead');
+const Create = require('../classes/dbCreate');
+
+const MAX_CARD_ID = 52;
 
 exports.showAll = (req, res)  => {
     Read.findAll('card')
@@ -21,6 +24,22 @@ exports.getCard = (req, res) => {
         return res.send(data);
     })
     .catch((error) => {
+        // TODO: Actual error handling
         return res.send(error);
     });
+};
+
+exports.initDeck = (req, res) => {
+    Create.insertIdOnly('deck','id')
+    .then((data) => {
+        for (let i=1; i<=MAX_CARD_ID; i++) {
+            var cols = ['id_card', 'id_deck'];
+            var values = [i, data.id];
+            Create.insert('deck_card',cols,values);
+        }
+    })
+    .catch((error) => {
+        // TODO: Actual error handling
+        return res.send(error);
+    })
 };
