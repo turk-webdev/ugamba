36e46ba744f5053dd44e0e21795c37ee42eeffa2
Turk Erdin <ufkun@erdin.org>
2020-11-17 12:49:03 -0800

    Simplifying init deck routes

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const deckController = require('../controllers/deck.controller.js');
 
 // Deck API calls
-router.get('/deck/card/all', deckController.showAll);
-router.get('/deck/cardId/:id', deckController.getCard);
-router.get('/deck/init/:gameId');
-router.get('/deck/deal/:gameId/:playerId');
+router.get('/deck/card/all', deckController.showAll);       // Get all possible cards
+router.get('/deck/cardId/:id', deckController.getCard);     // Get specific card by id
+router.get('/deck/init/:gameId', deckController.initDeck);  // Initialize a new deck for a game
+
 
 router.get('/', (request, response) => {
   response.send('Hello API');
