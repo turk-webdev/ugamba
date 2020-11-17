d7a1fc89eb9ef60e2a9b4d83474e48d35af3f941
Turk Erdin <ufkun@erdin.org>
2020-11-16 17:30:16 -0800

    Divided test READ calls to ok & not ok

--- routes/api.js
+++ routes/api.js
@@ -9,9 +9,12 @@ router.get('/deck/card/all', deckController.showAll);
 router.get('/deck/card/:id', deckController.showCard);
 
 // Test API calls
+// Tested & working
 router.get('/all/:table', testController.showAll);
 router.get('/one/single/exact/:table/:col/:query',testController.getOneExact);
 router.get('/any/single/exact/:table/:col/:query',testController.getAnyExact);
+
+// Tested & not working
 router.get('/one/single/like/:table/:col/:query',testController.getOneLike);
 router.get('/any/single/like/:table/:col/:query',testController.getAnyLike);
 
