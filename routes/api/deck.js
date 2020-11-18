71d70c10ebf660fc623c0bef6b3f2412b6799053
Turk Erdin <ufkun@erdin.org>
2020-11-18 15:35:59 -0800

    removed unnecessary routes

--- routes/api/deck.js
+++ routes/api/deck.js
@@ -4,13 +4,7 @@ const router = express.Router();
 const deckController = require('../../controllers/deck.controller.js');
 
 // Deck API calls
-router.get('/card/all', deckController.showAll);           // Get all possible cards
-router.get('/cardId/:id', deckController.getCard);         // Get specific card by id
-router.get('/init/:gameId', deckController.initDeck);      // Initialize a new deck for an existing game
+router.post('/init/:gameId', deckController.initDeckForGame);
 
-
-router.get('/', (request, response) => {
-  response.send('Hello API');
-});
 
 module.exports = router;
