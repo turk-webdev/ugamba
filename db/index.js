d9158a225dc12fac95ecce2d92a7f9185387e9ea
Turk Erdin <ufkun@erdin.org>
2020-11-18 15:35:42 -0800

    commented out debug console logs

--- db/index.js
+++ db/index.js
@@ -1,3 +1,10 @@
+// const initOptions = {
+//     query(e) {
+//         console.log(e.query);
+//     },
+// };
+
+// const pgp = require('pg-promise')(initOptions);
 const pgp = require('pg-promise')();
 
 const connection = pgp(process.env.DATABASE_URL);
