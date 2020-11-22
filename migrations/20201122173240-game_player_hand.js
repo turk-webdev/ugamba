8f0ba78d69e440800bc8dddcc4cabda99c290cfd
g.freedland <gfreedland@mail.sfsu.edu>
2020-11-22 10:08:06 -0800

    fixed game player hand table to include card ids

--- migrations/20201122173240-game_player_hand.js
+++ migrations/20201122173240-game_player_hand.js
@@ -10,6 +10,30 @@ module.exports = {
         type: Sequelize.INTEGER,
         primaryKey: true,
       },
+      deck_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      deck_card1_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      deck_card2_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      deck_card3_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      deck_card4_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      deck_card5_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
       strength: {
         type: Sequelize.INTEGER,
         allowNull: false,
