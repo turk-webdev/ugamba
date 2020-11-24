979e0382ec001f089a336b345b4138ac90455122
adamjbea <adamjbea@gmail.com>
2020-11-23 17:43:57 -0800

    more conflict resolution

--- routes/api/deck.js
+++ routes/api/deck.js
@@ -8,4 +8,5 @@ router.get('/init/:gameId', deckController.initDeckForGame);
 router.post('/deal', deckController.dealCardToPlayer); // Post body should include JSON with fields: id_game, id_game_player
 router.get('/table/:deckId', deckController.getTableCards);
 router.post('/reset', deckController.resetGameDeck); // Post body should include JSON with field: id_game
+
 module.exports = router;
