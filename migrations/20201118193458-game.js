c4f302601116e1170f57d54497d168dee4049c88
adamjbea <adamjbea@gmail.com>
2020-11-23 17:38:33 -0800

    Final migration changes
    pretty sure this is it fam

--- migrations/20201118193458-game.js
+++ migrations/20201118193458-game.js
@@ -1,33 +1,41 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable(
-      'game',
-      {  
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-        },
-        num_players: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          defaultValue: 1
-        },
-        id_deck: {
-          type: Sequelize.INTEGER,
-          allowNull: false
-        },
-        game_pot: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          defaultValue: 0
-        }
-      }
-    );
+    return queryInterface.createTable('game', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      num_players: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      id_deck: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      game_pot: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      min_bet: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      game_round: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      curr_game_player_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
   },
-  down: (queryInterface, Sequelize) => {
+  down: (queryInterface) => {
     return queryInterface.dropTable('game');
-  }
+  },
 };
--- routes/api/deck.js
+++ routes/api/deck.js
@@ -5,7 +5,6 @@ const deckController = require('../../controllers/deck.controller.js');
 
 // Deck API calls
 router.get('/init/:gameId', deckController.initDeckForGame);
-router.post('/deal', deckController.dealCardToPlayer);        // Body should include JSON with fields: id_game, id_game_player
-
+router.post('/deal', deckController.dealCardToPlayer); // Body should include JSON with fields: id_game, id_game_player
 
 module.exports = router;
