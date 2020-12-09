17640f1d6c00aca74c9997a65ff8de66f6d78d0a
Turk Erdin <ufkun@erdin.org>
2020-12-09 14:02:23 -0800

    Unit tests for the winBuilder.js

--- routes/tests.js
+++ routes/tests.js
@@ -1,3 +1,4 @@
+/* eslint-disable*/
 const express = require('express');
 
 const router = express.Router();
@@ -13,6 +14,62 @@ router.get('/', (_, response) => {
       console.log(error);
       response.json({ error });
     });
+});
+
+// Before posting here, you must create a game
+// Manually populate players into the game
+router.post('/init-game', (req,res) => {
+  // Adjust these based on your test game
+  const MY_PORT = 3000;
+  const GAME_ID = 0;
+  const PLAYER_IDS = [2,3,4,5];
+  const DEALER_ID = 6;    
+    fetch(
+      'http://localhost:'+MY_PORT+'/api/deck/init/0', 
+      { 
+        method: 'get', 
+        headers: { 'Content-Type': 'application/json'},
+      }).then(console.log)
+
+      // Put 5 cards for dealer
+      for (let i=0; i<5; i++) {
+        fetch(
+          'http://localhost:'+MY_PORT+'/api/deck/deal', 
+          { 
+            method: 'post', 
+            headers: { 'Content-Type': 'application/json'}, 
+            body: JSON.stringify({
+                id_game: GAME_ID,
+                id_game_player: DEALER_ID
+            })
+          }).then(console.log)
+      }
+
+      // Put 2 cards for each player
+      for (let i=0; i<2; i++) {
+        PLAYER_IDS.forEach(player => {
+          fetch(
+            'http://localhost:'+MY_PORT+'/api/deck/deal', 
+            { 
+              method: 'post', 
+              headers: { 'Content-Type': 'application/json'}, 
+              body: JSON.stringify({
+                  id_game: GAME_ID,
+                  id_game_player: player
+              })
+            }).then(console.log)
+        })
+      }
+
+      fetch(
+        'http://localhost:'+MY_PORT+'/tests/win', 
+        { 
+          method: 'post', 
+          headers: { 'Content-Type': 'application/json'}, 
+          body: JSON.stringify({
+              id_game: ID_GAME
+          })
+        }).then(console.log)
 });
 
 router.post('/win', (req,res) => {
