c15637488f4f91476605e79717fe27d0577c48a5
Francis Cruz <fcruz@blend.com>
2020-12-14 12:05:29 -0800

    bug

--- routes/api/game.js
+++ routes/api/game.js
@@ -5,11 +5,7 @@ const Game = require('../../controllers/game.controller.js');
 
 router.get('/:game_id', Game.joinGame);
 
-router.delete('/leave/:game_id', Game.leaveGame);
-
 router.post('/join', Game.createOrJoin);
-router.put('/fold/:game_id', Game.playerFold);
-router.put('/next/:game_id', Game.changeRound);
 router.post('/:game_id/:game_action', Game.actionHandler);
 
 module.exports = router;
